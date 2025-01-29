1ef21eec29991c1a2a04f7123112cf9aa74258a4
D-blac <a.damian.ng.od@gmail.com>
2025-01-29 17:35:14 +0100

    TEst

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// index.js
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3751;
 
-// Enable CORS for all origins
+
 app.use(cors("*"));
 
 app.get('/', (req, res) => {
     res.json({
         email: "obijekwudamian@gmailcom", 
         current_datetime: new Date().toISOString(),
-        github_url: "https://github.com/D-blac/HNG12-0-.git"
+        github_url: "https://github.com/D-blac/HNG12-0-"
     });
 });
 
